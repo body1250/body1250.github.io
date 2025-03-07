32ef15937007297278e8ed1ee6084cdcacacccff
abdarahman <body12500@gmail.com>
2025-03-07 14:03:38 +0300

    fix a problem

--- project.js
+++ project.js
@@ -16,7 +16,7 @@
   </div>
 </section>; */}
 
-export function makeskill(imgsource, alt, nameofproject, description, link){
+ function makeskill(imgsource, alt, nameofproject, description, link){
 
     let con_skills = document.querySelector('.con-skills')
     let skill = document.createElement("div")
@@ -57,7 +57,7 @@ export function makeskill(imgsource, alt, nameofproject, description, link){
     con_skills.appendChild(skill)
     
 }
-
+console.log(32)
 makeskill('images/google-search.png','search photo',"Google Search","search by google search with the advanced search and image search",'https://body1250.github.io/google-search/')
 makeskill('images/coffee-world.png','coffe shop photo',"Coffee shop","a store sells coffee products and coffee making tools",'https://body1250.github.io/coffee-world/')
 makeskill('images/replace-text.png','text photo',"replace text","write a paragraph an search about any word or letters and know how many times it repeats",'https://body1250.github.io/replace-text/')
